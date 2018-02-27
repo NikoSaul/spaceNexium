37eeb7450ed768bd5fee360f86308fd5e1e5e039
DESKTOP-PC0BNGV\BeyondDev <jdeletoile@laposte.net>
2018-02-27 16:33:04 +0100

    - Retrait complet de library (Untrack) - Début de scènes avec scripts globaux et lobby tout moche

---
+++ spaceNexium/Assets/I2/Localization/Scripts/Utils/PersistentStorage.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+using System;
+
+namespace I2.Loc
+{
+    public static class PersistentStorage
+    {
+#if UNITY_WEBGL || UNITY_WSA || NETFX_CORE
+        public static bool Save( string fileName, string data )
+        {
+            try
+            {
+                PlayerPrefs.SetString(fileName, data);
+                return true;
+            }
+            catch ( Exception )
+            {
+                Debug.LogError("Error saving PlayerPrefs " + fileName);
+                return false;
+            }
+        }
+
+        public static string Load( string fileName )
+        {
+            return PlayerPrefs.GetString(fileName, null);
+        }
+
+        public static void Delete( string fileName )
+        {
+            PlayerPrefs.DeleteKey(fileName);
+        }
+#else
+        public static bool Save(string fileName, string data)
+        {
+            try
+            {
+                var path = Application.persistentDataPath + "/" + fileName + ".loc";
+                System.IO.File.WriteAllText(path, data, System.Text.Encoding.UTF8 );
+                return true;
+            }
+            catch (Exception)
+            {
+                Debug.LogError("Error saving file " + fileName);
+                return false;
+            }
+        }
+
+        public static string Load(string fileName)
+        {
+            try
+            {
+                var path = Application.persistentDataPath + "/" + fileName + ".loc";
+                return System.IO.File.ReadAllText(path, System.Text.Encoding.UTF8);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static void Delete( string fileName )
+        {
+            try
+            {
+                var path = Application.persistentDataPath + "/" + fileName + ".loc";
+                System.IO.File.Delete(path);
+            }
+            catch (Exception)
+            {
+            }
+        }
+#endif
+
+
+        public static bool SaveFile(string fileName, string data)
+        {
+            try
+            {
+                //Debug.Log("Saving file: " + fileName);
+                System.IO.File.WriteAllText(fileName, data, System.Text.Encoding.UTF8);
+                return true;
+            }
+            catch (Exception)
+            {
+                Debug.LogError("Error saving file " + fileName);
+                return false;
+            }
+        }
+
+        public static string LoadFile(string fileName)
+        {
+            try
+            {
+                return System.IO.File.ReadAllText(fileName, System.Text.Encoding.UTF8);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public static void DeleteFile(string fileName)
+        {
+            try
+            {
+                System.IO.File.Delete(fileName);
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public static bool HasFile(string fileName)
+        {
+            return System.IO.File.Exists(fileName);
+        }
+
+    }
+}
---
+++ spaceNexium/Assets/script/Lobby/LobbyTabManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LobbyTabManager : MonoBehaviour
+{
+	public static LobbyTabManager instance { get; private set; }
+    
+	private void Awake()
+	{
+		instance = this;
+	}
+
+    #region First
+    
+
+    public void OpenFirstTab()
+    {
+
+    }
+
+    public void CloseFirstTab()
+    {
+
+    }
+    #endregion
+
+    #region Second
+    
+    public void OpenSecondTab()
+    {
+
+    }
+
+    public void CloseSecondTab()
+    {
+
+    }
+    #endregion
+
+    #region Third
+
+    internal void OpenThirdTab()
+    {
+
+    }
+
+    internal void CloseThirdTab()
+    {
+
+    }
+    #endregion
+
+    #region Fourth
+
+    internal void OpenFourthTab()
+    {
+
+    }
+
+    internal void CloseFourthTab()
+    {
+
+    }
+    #endregion
+}
