996835f0bf3d1bda2cd566b556ae2a5d083f8ec8
Niko <niikosaul@gmail.com>
2018-02-27 10:30:45 +0100

    Added: begin generator description

---
+++ spaceNexium/Assets/script/GenerateShip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class GenerateShip : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject moduleBase;
+
+    public string geneticCode;
+
+    private void Awake()
+    {
+        string[] genes = geneticCode.Split(' ');
+    
+        for (int i = 0; i < genes.Length; i+=3) {
+
+            
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ spaceNexium/Assets/script/Ship.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] cockpits;
+
+    [SerializeField]
+    private GameObject[] canons;
+
+    [SerializeField]
+    private GameObject[] bases;
+
+    [SerializeField]
+    private GameObject[] wings;
+
+    [SerializeField]
+    private GameObject[] protections;
+
+    [SerializeField]
+    private GameObject[] reactors;
+
+    // Use this for initialization
+    void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
